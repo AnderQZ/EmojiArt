6687c7a8df79024a569370d532ffe09727bbd6b1
“Your <lqizuan@outlook.com>
2022-01-09 18:16:09 +0800

    first commit

---
+++ EmojiArt/EmojiArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiArtApp.swift
+//  EmojiArt
+//
+//  Created by Ander Johnson on 9/1/2022.
+//
+
+import SwiftUI
+
+@main
+struct EmojiArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
