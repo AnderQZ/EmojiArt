68f5f1e2fcf33c13ec47633cb3d46e52316ee4db
“Your <lqizuan@outlook.com>
2022-01-10 19:20:57 +0800

    增加画板背景更换功能--safari拖拽图片进来

--- EmojiArt/EmojiArtDocumentView.swift
+++ EmojiArt/EmojiArtDocumentView.swift
@@ -22,28 +22,48 @@ struct EmojiArtDocumentView: View {
     var documentBody: some View {
         GeometryReader { geometry in
             ZStack {
-                Color.yellow
-                ForEach(document.emojis) { emoji in
-                    Text(emoji.text)
-                        .font(.system(size: fontSize(for: emoji)))
-                        .position(position(for: emoji, in: geometry))
+                Color.white.overlay(
+                    OptionalImage(uiImage: document.backgroundImage)
+                        .position(convertFromEmojiCoordinates((0, 0), in: geometry))
+                )
+                if document.backgroundImageFetchStatus == .fetching {
+                    ProgressView().scaleEffect(2)
+                } else {
+                    ForEach(document.emojis) { emoji in
+                        Text(emoji.text)
+                            .font(.system(size: fontSize(for: emoji)))
+                            .position(position(for: emoji, in: geometry))
+                    }
                 }
             }
-            .onDrop(of: [.plainText], isTargeted: nil) { providers, location in
-                 drop(providers: providers, at: location, in: geometry)
+            .onDrop(of: [.plainText,.url,.image], isTargeted: nil) { providers, location in
+                drop(providers: providers, at: location, in: geometry)
             }
         }
     }
     
     private func drop(providers: [NSItemProvider], at location: CGPoint, in geometry: GeometryProxy) -> Bool {
-        return providers.loadObjects(ofType: String.self) { string in
-            if let emoji = string.first, emoji.isEmoji {
-                document.addEmoji(
-                    String(emoji),
-                    at: convertToEmojiCoordinates(location, in: geometry),
-                    size: defaultEmojiFontSize)
+        var found = providers.loadObjects(ofType: URL.self) { url in
+            document.setBackground(EmojiArtModel.Background.url(url.imageURL))
+        }
+        if !found {
+            found = providers.loadObjects(ofType: UIImage.self) { image in
+                if let data = image.jpegData(compressionQuality: 1.0) {
+                    document.setBackground(.imageData(data))
+                }
             }
         }
+        if !found {
+            found = providers.loadObjects(ofType: String.self) { string in
+                if let emoji = string.first, emoji.isEmoji {
+                    document.addEmoji(
+                        String(emoji),
+                        at: convertToEmojiCoordinates(location, in: geometry),
+                        size: defaultEmojiFontSize)
+                }
+            }
+        }
+        return found
     }
     
     
@@ -79,7 +99,6 @@ struct EmojiArtDocumentView: View {
     }
     
     let testEmojis = "🤖💩🙄😪😺👽💋🦷👁🧕🦸‍♂️👠💍👑🧤🐶🐯🐧🐥🦄🐝🐛🦋🐌🐢"
-    
 }
 
 
--- EmojiArt/UtilityViews.swift
+++ EmojiArt/UtilityViews.swift
@@ -5,4 +5,14 @@
 //  Created by Ander Johnson on 9/1/2022.
 //
 
-import Foundation
+import SwiftUI
+
+struct OptionalImage: View {
+    var uiImage: UIImage?
+    
+    var body: some View {
+        if uiImage != nil {
+            Image(uiImage: uiImage!)
+        }
+    }
+}
